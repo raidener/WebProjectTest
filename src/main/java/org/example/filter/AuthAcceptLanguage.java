449eed13148bad38ed6436b2d28795fcb6ea07ab
Dima Golovnja <raidener@mail.ru>
2020-10-19 21:05:53 +0300

    Добавлена обработка ошибка 500

---
+++ src/main/java/org/example/filter/AuthAcceptLanguage.java
@@ -0,0 +1,38 @@
+package org.example.filter;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class AuthAcceptLanguage implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("In AuthAcceptLanguage");
+
+        HttpServletRequest castedRequest = (HttpServletRequest) request;
+        String authHeader = castedRequest.getHeader("Accept-Language");
+        if (StringUtils.isNotBlank(authHeader)) { //for future JWT Token Auth
+            System.out.println("Header In AuthAcceptLanguage was found with payload: " + authHeader);
+        } else {
+            System.out.println("Header In AuthAcceptLanguage was not found!");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/error500");
+            System.out.println("Forward will be done");
+            dispatcher.forward(request,response);
+        }
+        chain.doFilter(request, response);
+
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/main/webapp/WEB-INF/errors/500.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: DIMA
+  Date: 10/19/2020
+  Time: 8:41 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Ошибка 505</h1>
+</body>
+</html>
